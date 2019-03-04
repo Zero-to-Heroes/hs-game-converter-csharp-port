4ec39e3c6cc1fbd6cf78bef1ab1f6fe25a3d3eaa
sebastientromp <sebastien.tromp@gmail.com>
2019-03-04 16:06:23 +0100

    feature: add parsing for card received in hand

---
+++ HearthstoneReplays/Events/Parsers/ReceiveCardInHandParser.cs
@@ -0,0 +1,133 @@
+﻿using HearthstoneReplays.Parser;
+using HearthstoneReplays.Parser.ReplayData;
+using HearthstoneReplays.Parser.ReplayData.GameActions;
+using System;
+using HearthstoneReplays.Enums;
+using HearthstoneReplays.Parser.ReplayData.Entities;
+
+namespace HearthstoneReplays.Events.Parsers
+{
+    public class ReceiveCardInHandParser : ActionParser
+    {
+        private GameState GameState { get; set; }
+        private ParserState ParserState { get; set; }
+
+        public ReceiveCardInHandParser(ParserState ParserState)
+        {
+            this.ParserState = ParserState;
+            this.GameState = ParserState.GameState;
+        }
+
+        public bool AppliesOnNewNode(Node node)
+        {
+            return node.Type == typeof(TagChange)
+                && (node.Object as TagChange).Name == (int)GameTag.ZONE
+                && (node.Object as TagChange).Value == (int)Zone.HAND
+                && GameState.CurrentEntities[(node.Object as TagChange).Entity].GetTag(GameTag.ZONE) != (int)Zone.DECK;
+        }
+
+        public bool AppliesOnCloseNode(Node node)
+        {
+            var appliesToShowEntity = node.Type == typeof(ShowEntity)
+                && (node.Object as ShowEntity).GetTag(GameTag.ZONE) == (int)Zone.HAND
+                && (!GameState.CurrentEntities.ContainsKey((node.Object as ShowEntity).Entity)
+                    || GameState.CurrentEntities[(node.Object as ShowEntity).Entity].GetTag(GameTag.ZONE) != (int)Zone.DECK);
+            var appliesToFullEntity = node.Type == typeof(FullEntity)
+                && (node.Object as FullEntity).GetTag(GameTag.ZONE) == (int)Zone.HAND
+                && (!GameState.CurrentEntities.ContainsKey((node.Object as FullEntity).Id)
+                    || GameState.CurrentEntities[(node.Object as FullEntity).Id].GetTag(GameTag.ZONE) != (int)Zone.DECK);
+            return appliesToShowEntity || appliesToFullEntity;
+        }
+
+        public GameEventProvider CreateGameEventProviderFromNew(Node node)
+        {
+            var tagChange = node.Object as TagChange;
+            var entity = GameState.CurrentEntities[tagChange.Entity];
+            var cardId = entity.CardId;
+            var controllerId = entity.GetTag(GameTag.CONTROLLER);
+            var previousZone = entity.GetTag(GameTag.ZONE);
+            var provider = new GameEventProvider
+            {
+                Timestamp = DateTimeOffset.Parse(tagChange.TimeStamp),
+                SupplyGameEvent = () => new GameEvent
+                {
+                    Type = "RECEIVE_CARD_IN_HAND",
+                    Value = new
+                    {
+                        CardId = cardId,
+                        ControllerId = controllerId,
+                        LocalPlayer = ParserState.LocalPlayer,
+                        OpponentPlayer = ParserState.OpponentPlayer,
+                        PreviousZone = previousZone,
+                    }
+                },
+                NeedMetaData = true,
+                CreationLogLine = node.CreationLogLine
+            };
+            return provider;
+        }
+
+        public GameEventProvider CreateGameEventProviderFromClose(Node node)
+        {
+            if (node.Type == typeof(ShowEntity))
+            {
+                return CreateEventFromShowEntity(node.Object as ShowEntity, node.CreationLogLine);
+            }
+            else if (node.Type == typeof(FullEntity))
+            {
+                return CreateEventFromFullEntity(node.Object as FullEntity, node.CreationLogLine);
+            }
+            return null;
+        }
+
+        private GameEventProvider CreateEventFromShowEntity(ShowEntity showEntity, string creationLogLine)
+        {
+            var cardId = showEntity.CardId;
+            var controllerId = showEntity.GetTag(GameTag.CONTROLLER);
+            var previousZone = showEntity.GetTag(GameTag.ZONE);
+            return new GameEventProvider
+            {
+                Timestamp = DateTimeOffset.Parse(showEntity.TimeStamp),
+                SupplyGameEvent = () => new GameEvent
+                {
+                    Type = "RECEIVE_CARD_IN_HAND",
+                    Value = new
+                    {
+                        CardId = cardId,
+                        ControllerId = controllerId,
+                        LocalPlayer = ParserState.LocalPlayer,
+                        OpponentPlayer = ParserState.OpponentPlayer,
+                        PreviousZone = previousZone,
+                    }
+                },
+                NeedMetaData = true,
+                CreationLogLine = creationLogLine
+            };
+        }
+
+        private GameEventProvider CreateEventFromFullEntity(FullEntity fullEntity, string creationLogLine)
+        {
+            var cardId = fullEntity.CardId;
+            var controllerId = fullEntity.GetTag(GameTag.CONTROLLER);
+            var previousZone = fullEntity.GetTag(GameTag.ZONE);
+            return new GameEventProvider
+            {
+                Timestamp = DateTimeOffset.Parse(fullEntity.TimeStamp),
+                SupplyGameEvent = () => new GameEvent
+                {
+                    Type = "RECEIVE_CARD_IN_HAND",
+                    Value = new
+                    {
+                        CardId = cardId,
+                        ControllerId = controllerId,
+                        LocalPlayer = ParserState.LocalPlayer,
+                        OpponentPlayer = ParserState.OpponentPlayer,
+                        PreviousZone = previousZone,
+                    }
+                },
+                NeedMetaData = true,
+                CreationLogLine = creationLogLine
+            };
+        }
+    }
+}
