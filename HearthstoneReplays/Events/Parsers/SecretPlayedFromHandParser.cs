1bc211015bb949b628fcc908cc43ce9bbdb01ccf
sebastientromp <sebastien.tromp@gmail.com>
2019-03-04 16:06:12 +0100

    feature: add parsing for secret played

---
+++ HearthstoneReplays/Events/Parsers/SecretPlayedFromHandParser.cs
@@ -0,0 +1,69 @@
+﻿using HearthstoneReplays.Parser;
+using HearthstoneReplays.Parser.ReplayData;
+using HearthstoneReplays.Parser.ReplayData.GameActions;
+using System;
+using HearthstoneReplays.Enums;
+using HearthstoneReplays.Parser.ReplayData.Entities;
+
+namespace HearthstoneReplays.Events.Parsers
+{
+    public class SecretPlayedFromHandParser : ActionParser
+    {
+        private GameState GameState { get; set; }
+        private ParserState ParserState { get; set; }
+
+        public SecretPlayedFromHandParser(ParserState ParserState)
+        {
+            this.ParserState = ParserState;
+            this.GameState = ParserState.GameState;
+        }
+
+        public bool AppliesOnNewNode(Node node)
+        {
+            return node.Type == typeof(TagChange)
+                && (node.Object as TagChange).Name == (int)GameTag.ZONE
+                && (node.Object as TagChange).Value == (int)Zone.SECRET
+                && GameState.CurrentEntities[(node.Object as TagChange).Entity].GetTag(GameTag.ZONE) == (int)Zone.HAND;
+        }
+
+        public bool AppliesOnCloseNode(Node node)
+        {
+            return node.Type == typeof(Parser.ReplayData.GameActions.Action) 
+                && (node.Object as Parser.ReplayData.GameActions.Action).Type == (int)BlockType.PLAY;
+        }
+
+        public GameEventProvider CreateGameEventProviderFromNew(Node node)
+        {
+            var tagChange = node.Object as TagChange;
+            var entity = GameState.CurrentEntities[tagChange.Entity];
+            var cardId = entity.CardId;
+            var controllerId = entity.GetTag(GameTag.CONTROLLER);
+            if (GameState.CurrentEntities[tagChange.Entity].GetTag(GameTag.CARDTYPE) != (int)CardType.ENCHANTMENT)
+            {
+                return new GameEventProvider
+                {
+                    Timestamp = DateTimeOffset.Parse(tagChange.TimeStamp),
+                    SupplyGameEvent = () => new GameEvent
+                    {
+                        Type = "SECRET_PLAYED",
+                        Value = new
+                        {
+                            CardId = cardId,
+                            ControllerId = controllerId,
+                            LocalPlayer = ParserState.LocalPlayer,
+                            OpponentPlayer = ParserState.OpponentPlayer
+                        }
+                    },
+                    NeedMetaData = false,
+                    CreationLogLine = node.CreationLogLine
+                };
+            }
+            return null;
+        }
+
+        public GameEventProvider CreateGameEventProviderFromClose(Node node)
+        {
+            return null;
+        }
+    }
+}
