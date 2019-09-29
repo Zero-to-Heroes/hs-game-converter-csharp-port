db5682ecbdd5550040e12a7dea8fd8ef655fd57d
sebastientromp <sebastien.tromp@gmail.com>
2019-09-29 18:12:56 +0200

    fix FATIGUE_DAMAGE event

--- HearthstoneReplays/Events/Parsers/FatigueParser.cs
+++ HearthstoneReplays/Events/Parsers/FatigueParser.cs
@@ -42,7 +42,7 @@ namespace HearthstoneReplays.Events.Parsers
                     Type = "FATIGUE_DAMAGE",
                     Value = new
                     {
-                        PlayerId = entity.Id,
+                        EntityId = entity.Id,
                         LocalPlayer = ParserState.LocalPlayer,
                         OpponentPlayer = ParserState.OpponentPlayer,
                         FatigueDamage = fatigueDamage,
