325e73774492369ca2a24fb7e74129b51ddb7d0d
sebastientromp <sebastien.tromp@gmail.com>
2020-04-13 09:58:02 +0200

    BUG: fix BGS_COMBAT_START event

--- HearthstoneReplays/Events/Parsers/BattlegroundsStartOfCombatParser.cs
+++ HearthstoneReplays/Events/Parsers/BattlegroundsStartOfCombatParser.cs
@@ -25,7 +25,7 @@ namespace HearthstoneReplays.Events.Parsers
         {
             return ParserState.CurrentGame.GameType == (int)GameType.GT_BATTLEGROUNDS
                 && node.Type == typeof(TagChange)
-                && (node.Object as TagChange).Name == (int)GameTag.NEXT_STEP
+                && (node.Object as TagChange).Name == (int)GameTag.STEP
                 && (node.Object as TagChange).Value == (int)Step.MAIN_START_TRIGGERS
                 && GameState.GetGameEntity().GetTag(GameTag.BOARD_VISUAL_STATE) == 2;
         }
