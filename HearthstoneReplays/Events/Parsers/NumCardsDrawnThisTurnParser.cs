f2f2ac5e144aaafbca201dae4841c4d391d042c9
sebastientromp <sebastien.tromp@gmail.com>
2021-01-28 10:38:30 +0100

    send NUM_CARDS_DRAWN_THIS_TURN event

---
+++ HearthstoneReplays/Events/Parsers/NumCardsDrawnThisTurnParser.cs
@@ -0,0 +1,63 @@
+﻿using HearthstoneReplays.Parser;
+using HearthstoneReplays.Parser.ReplayData;
+using HearthstoneReplays.Parser.ReplayData.GameActions;
+using System;
+using HearthstoneReplays.Enums;
+using HearthstoneReplays.Parser.ReplayData.Entities;
+using System.Collections.Generic;
+
+namespace HearthstoneReplays.Events.Parsers
+{
+    public class NumCardsDrawnThisTurnParser : ActionParser
+    {
+        private GameState GameState { get; set; }
+        private ParserState ParserState { get; set; }
+
+        public NumCardsDrawnThisTurnParser(ParserState ParserState)
+        {
+            this.ParserState = ParserState;
+            this.GameState = ParserState.GameState;
+        }
+
+        public bool AppliesOnNewNode(Node node)
+        {
+            return node.Type == typeof(TagChange)
+                && (node.Object as TagChange).Name == (int)GameTag.NUM_CARDS_DRAWN_THIS_TURN;
+        }
+
+        public bool AppliesOnCloseNode(Node node)
+        {
+            return false;
+        }
+
+        public List<GameEventProvider> CreateGameEventProviderFromNew(Node node)
+        {
+            var tagChange = node.Object as TagChange;
+            var entity = GameState.CurrentEntities[tagChange.Entity];
+            var cardsDrawn = tagChange.Value;
+            var controllerId = entity.GetTag(GameTag.CONTROLLER);
+            var gameState = GameEvent.BuildGameState(ParserState, GameState, tagChange, null);
+            return new List<GameEventProvider> { GameEventProvider.Create(
+                tagChange.TimeStamp,
+                "NUM_CARDS_DRAWN_THIS_TURN",
+                GameEvent.CreateProvider(
+                    "NUM_CARDS_DRAWN_THIS_TURN",
+                    null,
+                    controllerId,
+                    entity.Id,
+                    ParserState,
+                    GameState,
+                    gameState,
+                    new {
+                        NumCardsDrawn = cardsDrawn,
+                    }),
+                true,
+                node) };
+        }
+
+        public List<GameEventProvider> CreateGameEventProviderFromClose(Node node)
+        {
+            return null;
+        }
+    }
+}
