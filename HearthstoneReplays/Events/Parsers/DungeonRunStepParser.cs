22447c395626748a41ac00eaa3931f5d662f99e9
sebastientromp <sebastien.tromp@gmail.com>
2021-02-08 09:22:38 +0100

    fix issue parsing solo adventure steps

--- HearthstoneReplays/Events/Parsers/DungeonRunStepParser.cs
+++ HearthstoneReplays/Events/Parsers/DungeonRunStepParser.cs
@@ -5,11 +5,14 @@ using System;
 using HearthstoneReplays.Enums;
 using HearthstoneReplays.Parser.ReplayData.Entities;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace HearthstoneReplays.Events.Parsers
 {
     public class DungeonRunStepParser : ActionParser
     {
+        private static readonly int STARTING_HEALTH = 15;
+
         private GameState GameState { get; set; }
         private ParserState ParserState { get; set; }
 
@@ -21,33 +24,50 @@ namespace HearthstoneReplays.Events.Parsers
 
         public bool AppliesOnNewNode(Node node)
         {
-            return node.Type == typeof(TagChange)
-                && (node.Object as TagChange).Name == (int)GameTag.HEALTH
-                && !string.IsNullOrWhiteSpace((node.Object as TagChange).DefChange);
+            return false;
+            //return node.Type == typeof(TagChange)
+            //    && (node.Object as TagChange).Name == (int)GameTag.HEALTH
+            //    && !string.IsNullOrWhiteSpace((node.Object as TagChange).DefChange);
         }
 
         public bool AppliesOnCloseNode(Node node)
         {
-            return false;
+            return node.Type == typeof(Parser.ReplayData.GameActions.Action)
+                && (node.Object as Parser.ReplayData.GameActions.Action).Data
+                    .Where(data => data.GetType() == typeof(TagChange))
+                    .Select(data => data as TagChange)
+                    .Where(tag => tag.Name == (int)GameTag.HEALTH && !string.IsNullOrWhiteSpace(tag.DefChange))
+                    .Count() > 0;
         }
 
         public List<GameEventProvider> CreateGameEventProviderFromNew(Node node)
         {
-            var tagChange = node.Object as TagChange;
-            var runStep = 1 + (tagChange.Value - 15) / 5;
+            return null;
+        }
+
+        public List<GameEventProvider> CreateGameEventProviderFromClose(Node node)
+        {
             return new List<GameEventProvider> { GameEventProvider.Create(
-                tagChange.TimeStamp,
+                (node.Object as Parser.ReplayData.GameActions.Action).TimeStamp,
                 "DUNGEON_RUN_STEP",
                 () => {
-                    if (ParserState.CurrentGame.ScenarioID != (int)Scenario.DUNGEON_RUN)
+                    if (ParserState.CurrentGame.ScenarioID != (int)Scenario.MONSTER_HUNT)
                     {
                         return null;
                     }
+                    var action = (node.Object as Parser.ReplayData.GameActions.Action);
                     var heroEntityId = ParserState.GetEntity(ParserState.LocalPlayer.Id).GetTag(GameTag.HERO_ENTITY);
-                    if (tagChange.Entity != heroEntityId)
-                    {
-                        return null;
-                    }
+                    var tagChange = action.Data
+                            .Where(data => data.GetType() == typeof(TagChange))
+                            .Select(data => data as TagChange)
+                            .Where(tag => tag.Name == (int)GameTag.HEALTH && !string.IsNullOrWhiteSpace(tag.DefChange))
+                            .Where(tag => tag.Entity == heroEntityId)
+                            .FirstOrDefault();
+                    // If no tag change for the main player, we use the starting health
+                    // The issue with computing things this way is that it's now possible
+                    // to emit the event multiple times over the course of a match
+                    var healthChangeDef = (tagChange != null ? tagChange.Value : ParserState.GetEntity(heroEntityId).GetTag(GameTag.HEALTH)) - STARTING_HEALTH;
+                    var runStep = 1 + healthChangeDef / 5;
                     return new GameEvent
                     {
                         Type = "DUNGEON_RUN_STEP",
@@ -56,11 +76,6 @@ namespace HearthstoneReplays.Events.Parsers
                 },
                 true,
                 node) };
-        }
-
-        public List<GameEventProvider> CreateGameEventProviderFromClose(Node node)
-        {
-            return null;
         }
     }
 }
--- HearthstoneReplays/Events/Parsers/RumbleRunStepParser.cs
+++ HearthstoneReplays/Events/Parsers/RumbleRunStepParser.cs
@@ -5,11 +5,14 @@ using System;
 using HearthstoneReplays.Enums;
 using HearthstoneReplays.Parser.ReplayData.Entities;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace HearthstoneReplays.Events.Parsers
 {
     public class RumbleRunStepParser : ActionParser
     {
+        private static readonly int STARTING_HEALTH = 20;
+
         private GameState GameState { get; set; }
         private ParserState ParserState { get; set; }
 
@@ -21,48 +24,58 @@ namespace HearthstoneReplays.Events.Parsers
 
         public bool AppliesOnNewNode(Node node)
         {
-            return node.Type == typeof(TagChange)
-                && (node.Object as TagChange).Name == (int)GameTag.HEALTH
-                && !string.IsNullOrWhiteSpace((node.Object as TagChange).DefChange);
+            return false;
+            //return node.Type == typeof(TagChange)
+            //    && (node.Object as TagChange).Name == (int)GameTag.HEALTH
+            //    && !string.IsNullOrWhiteSpace((node.Object as TagChange).DefChange);
         }
 
         public bool AppliesOnCloseNode(Node node)
         {
-            return false;
+            return node.Type == typeof(Parser.ReplayData.GameActions.Action)
+                && (node.Object as Parser.ReplayData.GameActions.Action).Data
+                    .Where(data => data.GetType() == typeof(TagChange))
+                    .Select(data => data as TagChange)
+                    .Where(tag => tag.Name == (int)GameTag.HEALTH && !string.IsNullOrWhiteSpace(tag.DefChange))
+                    .Count() > 0;
         }
 
         public List<GameEventProvider> CreateGameEventProviderFromNew(Node node)
         {
-            var tagChange = node.Object as TagChange; 
-            // The player starts with 20 Health, and gains an additional 5 Health per defeated boss, 
-            // up to 45 Health for the eighth, and final boss.
-            int runStep = 1 + (tagChange.Value - 20) / 5;
-            return new List<GameEventProvider> { GameEventProvider.Create(
-                    tagChange.TimeStamp,
-                    "RUMBLE_RUN_STEP",
-                    () => {
-                         if (ParserState.CurrentGame.ScenarioID != (int)Scenario.RUMBLE_RUN)
-                         {
-                             return null;
-                         }
-                         var heroEntityId = ParserState.GetEntity(ParserState.LocalPlayer.Id).GetTag(GameTag.HERO_ENTITY);
-                         if (tagChange.Entity != heroEntityId)
-                         {
-                             return null;
-                         }
-                         return new GameEvent
-                         {
-                             Type = "RUMBLE_RUN_STEP",
-                             Value = runStep
-                         };
-                    },
-                    true,
-                    node) };
+            return null;
         }
 
         public List<GameEventProvider> CreateGameEventProviderFromClose(Node node)
         {
-            return null;
+            return new List<GameEventProvider> { GameEventProvider.Create(
+                (node.Object as Parser.ReplayData.GameActions.Action).TimeStamp,
+                "RUMBLE_RUN_STEP",
+                () => {
+                    if (ParserState.CurrentGame.ScenarioID != (int)Scenario.MONSTER_HUNT)
+                    {
+                        return null;
+                    }
+                    var action = (node.Object as Parser.ReplayData.GameActions.Action);
+                    var heroEntityId = ParserState.GetEntity(ParserState.LocalPlayer.Id).GetTag(GameTag.HERO_ENTITY);
+                    var tagChange = action.Data
+                            .Where(data => data.GetType() == typeof(TagChange))
+                            .Select(data => data as TagChange)
+                            .Where(tag => tag.Name == (int)GameTag.HEALTH && !string.IsNullOrWhiteSpace(tag.DefChange))
+                            .Where(tag => tag.Entity == heroEntityId)
+                            .FirstOrDefault();
+                    // If no tag change for the main player, we use the starting health
+                    // The issue with computing things this way is that it's now possible
+                    // to emit the event multiple times over the course of a match
+                    var healthChangeDef = (tagChange != null ? tagChange.Value : ParserState.GetEntity(heroEntityId).GetTag(GameTag.HEALTH)) - STARTING_HEALTH;
+                    var runStep = 1 + healthChangeDef / 5;
+                    return new GameEvent
+                    {
+                        Type = "RUMBLE_RUN_STEP",
+                        Value = runStep
+                    };
+                },
+                true,
+                node) };
         }
     }
 }
