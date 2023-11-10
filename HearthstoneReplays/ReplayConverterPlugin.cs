be20a2d8c5dc4242b2f87949cbb5e8316a05e5b0
sebastientromp <sebastien.tromp@gmail.com>
2023-11-10 12:38:43 +0100

    fix recursive loops when serializing

--- HearthstoneReplays/ReplayConverterPlugin.cs
+++ HearthstoneReplays/ReplayConverterPlugin.cs
@@ -8,88 +8,93 @@ using System.Collections.Generic;
 
 namespace HearthstoneReplays
 {
-	public class ReplayConverterPlugin
-	{
-		// a global event that triggers with two parameters:
-		//
-		// plugin.get().onGlobalEvent.addListener(function(first, second) {
-		//  ...
-		// });
-		public event Action<object, object> onGlobalEvent;
-		public event Action<string> onGameEvent;
+    public class ReplayConverterPlugin
+    {
+        // a global event that triggers with two parameters:
+        //
+        // plugin.get().onGlobalEvent.addListener(function(first, second) {
+        //  ...
+        // });
+        public event Action<object, object> onGlobalEvent;
+        public event Action<string> onGameEvent;
 
 
-		// plugin.get().convertLogsToXml(xmlLogs, function(result) {
-		//   console.log(result);
-		// });
-		// 
-		// notice how we will always call the callback on a new thread
-		public void convertLogsToXml(string logs, Action<object> callback)
-		{
-			Logger.Log = onGlobalEvent;
+        // plugin.get().convertLogsToXml(xmlLogs, function(result) {
+        //   console.log(result);
+        // });
+        // 
+        // notice how we will always call the callback on a new thread
+        public void convertLogsToXml(string logs, Action<object> callback)
+        {
+            Logger.Log = onGlobalEvent;
 
-			if (callback == null)
-			{
-				onGlobalEvent("No callback, returning", "");
-				return;
-			}
+            if (callback == null)
+            {
+                onGlobalEvent("No callback, returning", "");
+                return;
+            }
 
-			Task.Run(() => {
-				try
-				{
-					string replayXml = new ReplayConverter().xmlFromLogs(logs);
-					onGlobalEvent("Serialized", replayXml.Length);
-					callback(replayXml);
-				}
-				catch (Exception e)
-				{
-					onGlobalEvent("Exception when parsing game " + e.GetBaseException(), logs);
-					callback(null);
-				}
-			});
-		}
+            Task.Run(() =>
+            {
+                try
+                {
+                    string replayXml = new ReplayConverter().xmlFromLogs(logs);
+                    onGlobalEvent("Serialized", replayXml.Length);
+                    callback(replayXml);
+                }
+                catch (Exception e)
+                {
+                    onGlobalEvent("Exception when parsing game " + e.GetBaseException(), logs);
+                    callback(null);
+                }
+            });
+        }
 
-		private ReplayParser parser = new ReplayParser();
+        private ReplayParser parser = new ReplayParser();
 
-		public void initRealtimeLogConversion(Action<object> callback)
-		{
-			Logger.Log = onGlobalEvent;
-            GameEventHandler.EventProvider = (GameEvent gameEvent) => onGameEvent(JsonConvert.SerializeObject(gameEvent));
-			// The goal here is to make sure the order is kept while in Dev mode, where events are very rapidly firing
+        public void initRealtimeLogConversion(Action<object> callback)
+        {
+            Logger.Log = onGlobalEvent;
+            GameEventHandler.EventProvider = (GameEvent gameEvent) => onGameEvent(JsonConvert.SerializeObject(gameEvent, new JsonSerializerSettings()
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+            }));
+            // The goal here is to make sure the order is kept while in Dev mode, where events are very rapidly firing
             GameEventHandler.EventProviderAll = (IList<GameEvent> gameEvents) => onGameEvent(JsonConvert.SerializeObject(gameEvents));
             parser = new ReplayParser();
             parser.Init();
             callback?.Invoke(null);
         }
 
-		public void realtimeLogProcessing(string[] logLines, Action<object> callback)
-		{
-			Task.Run(() => {
-				try
-				{
-					Array.ForEach(logLines, logLine => parser.ReadLine(logLine));
-					callback(null);
-				}
-				catch (Exception e)
-				{
-                    Logger.Log("Exception when parsing game " + e?.GetBaseException() 
+        public void realtimeLogProcessing(string[] logLines, Action<object> callback)
+        {
+            Task.Run(() =>
+            {
+                try
+                {
+                    Array.ForEach(logLines, logLine => parser.ReadLine(logLine));
+                    callback(null);
+                }
+                catch (Exception e)
+                {
+                    Logger.Log("Exception when parsing game " + e?.GetBaseException()
                                 + " // " + logLines[logLines.Length - 1], "");
                     callback(null);
                 }
-			});
-		}
-		
-		public void triggerGlobalEvent(string first, string second)
-		{
-			if (onGlobalEvent == null)
-			{
-				return;
-			}
+            });
+        }
 
-			Task.Run(() =>
-			{
-				onGlobalEvent(first, second);
-			});
+        public void triggerGlobalEvent(string first, string second)
+        {
+            if (onGlobalEvent == null)
+            {
+                return;
+            }
+
+            Task.Run(() =>
+            {
+                onGlobalEvent(first, second);
+            });
         }
 
         //      public void startDevMode()
