ea7d824540d55466793c9242c9e600ec867fce99
sebastientromp <sebastien.tromp@gmail.com>
2026-01-13 14:16:16 +0100

    fix hero power changed sometimes not being sent

--- HearthstoneReplays/Events/Parsers/HeroPowerChangedParser.cs
+++ HearthstoneReplays/Events/Parsers/HeroPowerChangedParser.cs
@@ -26,11 +26,15 @@ namespace HearthstoneReplays.Events.Parsers
 
         public bool AppliesOnCloseNode(Node node, StateType stateType)
         {
-            return stateType == StateType.PowerTaskList
+            var appliesOnFullEntity = stateType == StateType.PowerTaskList
                 && node.Type == typeof(FullEntity)
                 && (node.Object as FullEntity).GetTag(GameTag.ZONE) == (int)Zone.PLAY
                 && (node.Object as FullEntity).GetTag(GameTag.CARDTYPE) == (int)CardType.HERO_POWER;
-                //&& !ParserState.ReconnectionOngoing;
+            var appliesOnShowEntity = stateType == StateType.PowerTaskList
+                && node.Type == typeof(ShowEntity)
+                && (node.Object as ShowEntity).GetTag(GameTag.ZONE) == (int)Zone.PLAY
+                && (node.Object as ShowEntity).GetTag(GameTag.CARDTYPE) == (int)CardType.HERO_POWER;
+            return appliesOnFullEntity || appliesOnShowEntity;
         }
 
         public List<GameEventProvider> CreateGameEventProviderFromNew(Node node)
@@ -40,6 +44,19 @@ namespace HearthstoneReplays.Events.Parsers
 
         public List<GameEventProvider> CreateGameEventProviderFromClose(Node node)
         {
+            if (node.Object is FullEntity)
+            {
+                return CreateFromFullEntity(node);
+            }
+            else if (node.Object is ShowEntity)
+            {
+                return CreateFromShowEntity(node);
+            }
+            return null;
+        }
+
+        private List<GameEventProvider> CreateFromFullEntity(Node node)
+        { 
             var fullEntity = node.Object as FullEntity;
             var cardId = fullEntity.CardId;
             var controllerId = fullEntity.GetEffectiveController();
@@ -56,6 +73,35 @@ namespace HearthstoneReplays.Events.Parsers
                     cardId,
                     controllerId,
                     fullEntity.Id,
+                    StateFacade,
+                    //gameState,
+                    new {
+                        CreatorCardId = creatorEntityCardId,
+                    }
+                ),
+                true,
+                node) };
+        }
+
+
+        private List<GameEventProvider> CreateFromShowEntity(Node node)
+        {
+            var fullEntity = node.Object as ShowEntity;
+            var cardId = fullEntity.CardId;
+            var controllerId = fullEntity.GetEffectiveController();
+            //var gameState = GameEvent.BuildGameState(ParserState, StateFacade, GameState, null, null);
+            var creatorEntityId = fullEntity.GetTag(GameTag.CREATOR);
+            var creatorEntityCardId = GameState.CurrentEntities.ContainsKey(creatorEntityId)
+                ? GameState.CurrentEntities[creatorEntityId].CardId
+                : null;
+            return new List<GameEventProvider> { GameEventProvider.Create(
+                fullEntity.TimeStamp,
+                "HERO_POWER_CHANGED",
+                GameEvent.CreateProvider(
+                    "HERO_POWER_CHANGED",
+                    cardId,
+                    controllerId,
+                    fullEntity.Entity,
                     StateFacade,
                     //gameState,
                     new {
